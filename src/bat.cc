40393b424fd70bba8ddbf2adce3589459713963e
Chiu Tai Lin <ctl800303@gmail.com>
2020-04-25 00:13:26 +0800

    Transit solution bits in a row

--- src/bat.cc
+++ src/bat.cc
@@ -59,14 +59,10 @@ void echolocation(vector<Bat> &bats, Bat leader) {
 }
 
 void Bat::movement(Bat leader) {
-    for (int each_bit = 0; each_bit < this->distribution.size(); ++each_bit) {
-        this->distribution[each_bit] = this->position[each_bit] + (2 * ((double)rand() / (RAND_MAX + 1.0)) - 1) * ((double)rand() / (RAND_MAX + 1.0));
-        if ((double)rand() / (RAND_MAX + 1.0) < 1 / (1 + exp(-this->distribution[each_bit])))
-            // this->candidate[each_bit] = ~this->candidate[each_bit];
-            this->candidate[each_bit] = 1;
-            // this->candidate[each_bit] = leader.position[each_bit];
-        else
-            this->candidate[each_bit] = 0;
+    for (int index = rand() % this->position.size(), round = 0; round < 50; ++index, ++round) {
+        this->distribution[index % this->position.size()] = this->position[index % this->position.size()] + (2 * ((double)rand() / (RAND_MAX + 1.0)) - 1) * ((double)rand() / (RAND_MAX + 1.0));
+        if ((double)rand() / (RAND_MAX + 1.0) < 1 / (1 + exp(-this->distribution[index % this->position.size()])))
+            this->candidate[index % this->position.size()] = ~this->candidate[index % this->position.size()];
     }
 
     if ((double)rand() / (RAND_MAX + 1.0) > pulse_rate) {
@@ -78,18 +74,15 @@ void Bat::movement(Bat leader) {
     for (int each_bit = 0; each_bit < this->velocity.size(); ++each_bit) {
         this->velocity[each_bit] = this->velocity[each_bit] + (this->position[each_bit] - leader.position[each_bit]) * frequency;
     }
-    for (int each_bit = 0; each_bit < this->distribution.size(); ++each_bit) {
-        this->distribution[each_bit] = this->velocity[each_bit];
-        if ((double)rand() / (RAND_MAX + 1.0) < (1 / (1 + exp(-this->distribution[each_bit]))))
-            this->candidate[each_bit] = ~this->candidate[each_bit];
-        //     this->candidate[each_bit] = 1;
-        // else
-        //     this->candidate[each_bit] = 0;
+    for (int index = rand() % this->position.size(), round = 0; round < 50; ++index, ++round) {
+        this->distribution[index % this->position.size()] = this->velocity[index % this->position.size()];
+        if ((double)rand() / (RAND_MAX + 1.0) < (1 / (1 + exp(-this->distribution[index % this->position.size()]))))
+            this->candidate[index % this->position.size()] = ~this->candidate[index % this->position.size()];
     }
 
     this->candidate_fitness = cost_evaluation(this->candidate);
 
-    if ( (double)rand() / (RAND_MAX + 1.0) < loudness) {// && this->candidate_fitness <= leader.fitness) {
+    if ( (double)rand() / (RAND_MAX + 1.0) < loudness && this->candidate_fitness <= leader.fitness) {
         this->position = this->candidate;
         this->fitness = this->candidate_fitness;
     }
